9839d1137aafa7706ba52899f4ca156d61c7201e
Chloe Urisohn <chloeurisohn@gmail.com>
2021-05-25 12:30:34 +0100

    add player factory

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 // The Gameboard Module
 const gameBoard = (function () {
     'use strict'
-    const sayHi = () => console.log('This is a new gameboard!');
+    const sayHi = () => console.log('Hi, this is a new gameboard!');
     const generateBoard = () => {
         const container = document.querySelector('#container');
         for (let i = 0; i < 9; i++) {
             const content = document.createElement('div');
             content.classList.add('box');
             container.appendChild(content);
-            gameArray.push(gameObject);
-            content.setAttribute("id", i);
+            content.setAttribute("id", i);        
         } // end of for loop
     }
-    console.log(container)
     const playGame = () => {
         console.log('Playing the game ...')
     }
+    playGame();
     return { sayHi, generateBoard };
 })();
 
-let gameArray = [];
-const gameObject = {
-    player1Mark: 'x',
-    player2Mark: 'o'
-};
 
 gameBoard.sayHi();
 gameBoard.generateBoard();
 
 // Event LISTENERS
 const boxes = Array.from(document.getElementsByClassName('box'));
-boxes.forEach((box) => {box.addEventListener('click', addMark);});
-player1Turn = true;
+boxes.forEach((box) => {box.addEventListener('click', addPlayerMark);});
+let player1Turn = true;
 
-function addMark(e) {
-    // player factory
-    const playerFactory = mark => {
-        const addXMark = () => {
-            console.log(`Player ${mark} is here`);
-            e.target.innerText = mark;
-        };
-        return { addXMark };
-    };
+// player factory
+const playerFactory = mark => {
+    const addMark = () => {
+    console.log(`${mark} Selected`);
+};
+return { addMark };
+};
+
+
 player1 = playerFactory('x');
-player1.addXMark();
+player2 = playerFactory('o');
+console.log(player1Turn);
+
+function addPlayerMark(e) { //on click event
+    console.log(player1Turn);
+    if (player1Turn) {
+        e.target.innerText = 'x';
+        player1.addMark();
+        player1Turn = false;
+        console.log(player1Turn);
+    }
+    else {
+        e.target.innerText = 'o';
+        player2.addMark();
+        player1Turn = true;
+        console.log(player1Turn);
+    }
 };
