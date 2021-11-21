519905539db7a76dc137a548bbe4c4412114692e
Ghost <chloeurisohn@gmail.com>
2021-11-21 10:54:33 +0000

    fIX BUG

--- script.js
+++ script.js
@@ -36,7 +36,10 @@ const gameBoard = (function () {
             ["2", "4", "6"],
             ["2", "5", "8"],
             ["0", "3", "6"],
-            ["1", "4", "7"]
+            ["1", "4", "7"],
+            ["0", "1", "2"],
+            ["3", "4", "5"],
+            ["6", "7", "8"]
         ];
         winningCombinations.forEach((combination) => {
             const filteredArray = arr.filter(val => combination.includes(val));
